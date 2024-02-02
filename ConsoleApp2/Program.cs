8acdd2739942d1d36655e9050b1e5224b2f65d20
Sistemas2-10 <Sistemas2-10@DESKTOP-V26RTMV>
2024-02-02 14:38:25 -0600

    Agregar archivos de proyecto.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,18 @@
+﻿Double[] Sueldo;
+Sueldo = new Double[5];
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine("ingrese el sueldo del empelado" + (i + 1));
+    Sueldo[i] = Double.Parse(Console.ReadLine());
+}
+    Console.WriteLine("Los sueldos ingresado son:");
+    for (int i = 0; i < 5; i++)
+    {
+        Console.Write("\t" + Sueldo[i]);
+
+    }
+  Console.ReadKey();
+
+
+
+
